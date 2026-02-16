b266a27379efcdbdf2cbfe4a32aa8f3e58b88445
cashtowme-png <cashtowme@gmail.com>
2026-02-16 13:12:25 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
 const CACHE_NAME = "ctmd-v1";
 const FILES_TO_CACHE = [
-  "/driver.html",
-  "/manifest.json"
+  "driver.html",
+  "manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
-    ))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
